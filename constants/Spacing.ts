4057727da1e5b2dd843912eb1fc70301e920b09a
ghost2023 <nathnaelwonisha@gmail.com>
2025-03-08 12:49:27 +0300

    feat: update colors

--- constants/Spacing.ts
+++ constants/Spacing.ts
@@ -7,3 +7,12 @@ export const spacing = {
   xxl: 48,
   xxxl: 56,
 } as const;
+
+export const BORDER_RADIUS = {
+  small: 6,
+  medium: 10,
+  default: 12,
+  large: 16,
+  xl: 18,
+  xxl: 20,
+} as const;
