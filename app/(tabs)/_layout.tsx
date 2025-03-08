b151a3b6abe4412b211dc3e753a5c7ba69ebd137
ghost2023 <nathnaelwonisha@gmail.com>
2025-03-08 18:32:57 +0300

    feat: add page to tab layout

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,18 +2,13 @@ import React from "react";
 import { Tabs } from "expo-router";
 
 import Colors from "@/constants/Colors";
-import { useClientOnlyValue } from "@/components/useClientOnlyValue";
-import { AntDesign, Feather } from "@expo/vector-icons";
-import { Text } from "@/components/ui/Text";
+import { Feather } from "@expo/vector-icons";
 
 export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
         tabBarActiveTintColor: Colors["light"].primary,
-        // Disable the static render of the header on web
-        // to prevent a hydration error in React Navigation v6.
-        headerShown: useClientOnlyValue(false, true),
         tabBarLabelStyle: {
           fontSize: 14,
           fontFamily: "medium",
@@ -27,6 +22,28 @@ export default function TabLayout() {
           headerShown: false,
           tabBarIcon: ({ color }) => (
             <Feather name="home" size={20} color={color} />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="my-equbs"
+        options={{
+          title: "My Equbs",
+          headerShown: false,
+          tabBarIcon: ({ color }) => (
+            <Feather name="bookmark" size={20} color={color} />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="browse"
+        options={{
+          title: "Equbs",
+          headerShown: false,
+          tabBarIcon: ({ color }) => (
+            <Feather name="search" size={20} color={color} />
           ),
         }}
       />
