57d738b02a8c970e33680caea17a60b646e5ba49
ghost2023 <nathnaelwonisha@gmail.com>
2025-03-15 16:33:05 +0300

    feat: uses the api call

--- components/Home/HomeCarousel.tsx
+++ components/Home/HomeCarousel.tsx
@@ -5,43 +5,9 @@ import { Feather } from "@expo/vector-icons";
 import { useQuery } from "@tanstack/react-query";
 import { ScrollView, View, useWindowDimensions } from "react-native";
 import { Skeleton } from "../ui/skeleton";
-
-// import * as React from "react";
-// import { View, useWindowDimensions } from "react-native";
-// import Carousel from "react-native-reanimated-carousel";
-// import { Text } from "../ui/Text";
-// import Colors from "@/constants/Colors";
-// import { Feather } from "@expo/vector-icons";
-
-const currentEqub = [
-  {
-    id: 1,
-    totalAmount: 100000,
-    depositedAmount: 200,
-    depositCycleDuration: 2,
-    depositCycleUnit: "Days",
-    withdrawCycleDuration: 2,
-    withdrawCycleUnit: "Weeks",
-  },
-  {
-    id: 2,
-    totalAmount: 100000,
-    depositedAmount: 200,
-    depositCycleDuration: 2,
-    depositCycleUnit: "Days",
-    withdrawCycleDuration: 2,
-    withdrawCycleUnit: "Weeks",
-  },
-  {
-    id: 3,
-    totalAmount: 100000,
-    depositedAmount: 200,
-    depositCycleDuration: 2,
-    depositCycleUnit: "Days",
-    withdrawCycleDuration: 2,
-    withdrawCycleUnit: "Weeks",
-  },
-];
+import { Equb } from "@/lib/types";
+import axios from "axios";
+import { env } from "@/lib/env";
 
 export default function HomeCarousel() {
   const color = Colors["light"];
@@ -50,9 +16,9 @@ export default function HomeCarousel() {
   const equbQuery = useQuery({
     queryKey: ["equb"],
     queryFn: async () => {
-      await new Promise((resolve) => setTimeout(resolve, 500));
+      const res = await axios.get(`${env.EXPO_PUBLIC_API_URL}/api/equbs`);
 
-      return currentEqub;
+      return res.data as Equb[];
     },
   });
 
@@ -105,7 +71,7 @@ export default function HomeCarousel() {
                   fontSize: 24,
                 }}
               >
-                {equb.totalAmount.toLocaleString("en", {
+                {equb.winningAmount.toLocaleString("en", {
                   currency: "ETB",
                   style: "currency",
                 })}
@@ -117,7 +83,7 @@ export default function HomeCarousel() {
                   fontWeight: 500,
                 }}
               >
-                every {equb.withdrawCycleDuration} {equb.withdrawCycleUnit}
+                every {equb.winningFrequency}
               </Text>
             </View>
             <View
@@ -147,7 +113,7 @@ export default function HomeCarousel() {
                     fontSize: 12,
                   }}
                 >
-                  {equb.depositedAmount.toLocaleString("en", {
+                  {equb.depositAmount.toLocaleString("en", {
                     currency: "ETB",
                     style: "currency",
                   })}
@@ -169,7 +135,7 @@ export default function HomeCarousel() {
                     fontSize: 12,
                   }}
                 >
-                  {equb.depositCycleDuration} {equb.depositCycleUnit}
+                  {equb.depositFrequency}
                 </Text>
               </View>
             </View>
@@ -179,104 +145,3 @@ export default function HomeCarousel() {
     </ScrollView>
   );
 }
-
-// export default function HomeCarousel() {
-//   const width = useWindowDimensions().width;
-//   const color = Colors.light;
-//
-//   return (
-//     <View>
-//       <Carousel
-//         width={width}
-//         snapEnabled={true}
-//         loop={false}
-//         pagingEnabled={true}
-//         data={currentEqub}
-//         height={140}
-//         renderItem={({ item }) => (
-//           <View
-//             key={item.id}
-//             style={{
-//               backgroundColor: color.background,
-//               paddingInline: 12,
-//               marginInline: 12,
-//
-//               elevation: 2,
-//               borderRadius: 12,
-//               position: "relative",
-//             }}
-//           >
-//             <View style={{ alignItems: "center", gap: 2, marginVertical: 12 }}>
-//               <Text
-//                 style={{
-//                   color: color.primary,
-//                   fontWeight: 500,
-//                   fontSize: 24,
-//                 }}
-//               >
-//                 {item.totalAmount.toLocaleString("en", {
-//                   currency: "ETB",
-//                   style: "currency",
-//                 })}
-//               </Text>
-//
-//               <Text
-//                 style={{
-//                   fontSize: 14,
-//                   fontWeight: 500,
-//                 }}
-//               >
-//                 every {item.withdrawCycleDuration} {item.withdrawCycleUnit}
-//               </Text>
-//             </View>
-//             <View
-//               style={{
-//                 flexDirection: "row",
-//                 justifyContent: "space-between",
-//                 marginTop: 2,
-//                 paddingVertical: 12,
-//               }}
-//             >
-//               <View
-//                 style={{ gap: 6, flexDirection: "row", alignItems: "center" }}
-//               >
-//                 <Feather
-//                   name="download"
-//                   size={12}
-//                   color={color.secondaryText}
-//                 />
-//                 <Text
-//                   style={{
-//                     color: color.secondaryText,
-//                     fontWeight: 500,
-//                     fontSize: 12,
-//                   }}
-//                 >
-//                   {item.depositedAmount.toLocaleString("en", {
-//                     currency: "ETB",
-//                     style: "currency",
-//                   })}
-//                 </Text>
-//               </View>
-//
-//               <View
-//                 style={{ gap: 6, flexDirection: "row", alignItems: "center" }}
-//               >
-//                 <Feather name="clock" size={12} color={color.secondaryText} />
-//                 <Text
-//                   style={{
-//                     color: color.secondaryText,
-//                     fontWeight: 500,
-//                     fontSize: 12,
-//                   }}
-//                 >
-//                   {item.depositCycleDuration} {item.depositCycleUnit}
-//                 </Text>
-//               </View>
-//             </View>
-//           </View>
-//         )}
-//       />
-//     </View>
-//   );
-// }
