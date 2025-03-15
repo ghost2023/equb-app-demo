38e0964dec65ea68c3854b8aafa4e1ff27397f9a
ghost2023 <nathnaelwonisha@gmail.com>
2025-03-15 16:30:31 +0300

    feat: uses the api call

--- components/Home/CurrentEqubList.tsx
+++ components/Home/CurrentEqubList.tsx
@@ -1,47 +1,17 @@
 import EqubCard, { EqubCardSkeleton } from "@/components/HomeEquibCard";
+import { env } from "@/lib/env";
+import { Equb } from "@/lib/types";
 import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 import { View } from "react-native";
-
-const currentEqub = [
-  {
-    id: 1,
-    totalAmount: 100000,
-    depositedAmount: 200,
-    depositCycleDuration: 2,
-    depositCycleUnit: "Days",
-    withdrawCycleDuration: 2,
-    withdrawCycleUnit: "Weeks",
-    members: 10,
-  },
-  {
-    id: 2,
-    totalAmount: 100000,
-    depositedAmount: 200,
-    depositCycleDuration: 2,
-    depositCycleUnit: "Days",
-    withdrawCycleDuration: 2,
-    withdrawCycleUnit: "Weeks",
-    members: 10,
-  },
-  {
-    id: 3,
-    totalAmount: 100000,
-    depositedAmount: 200,
-    depositCycleDuration: 2,
-    depositCycleUnit: "Days",
-    withdrawCycleDuration: 2,
-    withdrawCycleUnit: "Weeks",
-    members: 10,
-  },
-];
 
 const CurrentEqubList = () => {
   const equbQuery = useQuery({
     queryKey: ["equb"],
     queryFn: async () => {
-      await new Promise((resolve) => setTimeout(resolve, 4000));
+      const res = await axios.get(`${env.EXPO_PUBLIC_API_URL}/api/equbs`);
 
-      return currentEqub;
+      return res.data as Equb[];
     },
   });
 
