fd4fe12a3ddbba83e1c0d4cd0516f0d448f5535b
ghost2023 <nathnaelwonisha@gmail.com>
2025-03-16 11:15:06 +0300

    feat: add format days

--- lib/misc.ts
+++ lib/misc.ts
@@ -11,3 +11,18 @@ export function debounce(func: Function, wait: number | undefined = 400) {
     timeout = setTimeout(later, wait);
   };
 }
+
+/**
+ * parses a string into a number
+ * @param str
+ * @returns
+ */
+export function formatDays(days: number) {
+  if (days < 7 || days % 7 !== 0) {
+    return `${days} day${days > 1 ? "s" : ""}`;
+  }
+  if (days < 30) {
+    return `${Math.floor(days / 7)} week${Math.floor(days / 7) > 1 ? "s" : ""}`;
+  }
+  return `${Math.floor(days / 30)} month${Math.floor(days / 30) > 1 ? "s" : ""}${days % 30 > 0 ? " and " + (days % 30) + " day" + (days % 30 > 1 ? "s" : "") : ""}`;
+}
